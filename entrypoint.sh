b38f747c8d4819d1b800cc004b31459f48515285
Andrew Minkin <andrew.minkin@percona.com>
2022-07-15 16:48:56 +0600

    Use env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 git clone --depth 1 $1 doc
 cd doc/docs/release-notes
 version="${2/refs\/tags\/v//}"
-changelog=`cat ./$version.md`
-echo "::set-output changelog=$changelog"
+CHANGELOG=`cat ./$version.md`
+echo "CHANGELOG<<EOF" >> $GITHUB_ENV
+echo "$CHANGELOG" >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
