c2ae334ca5edee311ec0f89b58343bc485d5151f
Andrew Minkin <andrew.minkin@percona.com>
2022-07-15 16:10:06 +0600

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 git clone --depth 1 $1 doc
 cd doc/docs/release-notes
-version="${2/v//}"
+tag="${2##*/}"
+version="${tag/v//}"
 changelog=`cat ./$version.md`
 echo "::set-output changelog=$changelog"
